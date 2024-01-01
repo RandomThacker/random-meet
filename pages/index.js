30cc9f6e07e77752416f0300cc6635b836482214
RandomThacker <randomthacker@gmail.com>
2024-01-01 12:33:38 +0530

    Setup socket.io

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,14 @@
+import { useSocket } from "@/context/socket";
+import { useEffect } from "react";
+
 export default function Home() {
+  const socket = useSocket()
+  useEffect(()=>{
+    socket?.on("connect",()=>{
+      console.log((socket.id));
+    })
+  },[socket])
   return (
     <main>Hello World</main>
-   
   )
 }
