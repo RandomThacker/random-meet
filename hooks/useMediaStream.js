61fcb703f1a6936731042164c0340d07a28691fb
RandomThacker <randomthacker@gmail.com>
2024-01-02 13:46:20 +0530

    video rendering completed

---
+++ hooks/useMediaStream.js
@@ -0,0 +1,33 @@
+import {useState, useEffect, useRef} from 'react'
+
+const useMediaStream = () => {
+    const [state, setState] = useState(null)
+    const isStreamSet = useRef(false)
+
+
+    useEffect(() => {
+        if (isStreamSet.current) return;
+        isStreamSet.current = true;
+            (async function initStream() {
+                try {
+                    const stream = await navigator.mediaDevices.getUserMedia({
+                        audio: true,
+                        video: true
+          
+          
+                    })
+                    console.log("setting your stream");
+                    setState(stream)
+                }
+                catch (e) {
+                    console.log("error while streaiming", e)
+                }
+            })()
+    }, [])
+
+    return {
+        stream: state
+    }
+}
+
+export default useMediaStream
--- pages/[roomid].js
+++ pages/[roomid].js
@@ -1,9 +1,18 @@
 import { useSocket } from "@/context/socket";
 import usePeer from "@/hooks/usePeer";
+import useMediaStream from "@/hooks/useMediaStream";
+import Player from "@/component/Player";
 
-const Room = () =>{
+const Room = () => {
     const socket = useSocket()
-    const {peer,myId} = usePeer()
-}
+    const { peer, myId } = usePeer()
+    const { stream } = useMediaStream()
+
+    return (
+        <div>
+            <Player url={stream} muted playing playerId ={myId}/>        
+        </div>
+    )
+};
 
 export default Room
