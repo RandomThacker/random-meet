faf4ea930e07ec2e154456bbf636e3314d2d57c2
RandomThacker <randomthacker@gmail.com>
2024-02-01 18:43:56 +0530

    updated socket

--- context/socket.js
+++ context/socket.js
@@ -2,7 +2,10 @@ import { createContext, useContext, useEffect, useState } from "react";
 import { io } from "socket.io-client";
 
 const SocketContext = createContext(null);
-
+const URL =
+  process.env.NODE_ENV === "production"
+    ? "https://randommeet-server.onrender.com"
+    : "http://localhost:5000";
 export const useSocket = () => {
     const socket = useContext(SocketContext)
     return socket
@@ -13,15 +16,10 @@ export const SocketProvider = (props) => {
   const [socket, setSocket] = useState(null);
 
   useEffect(() => {
-    const connection = io();
+    const connection = io(URL);
     console.log("socket connection", connection)
     setSocket(connection);
   }, []);
-
-  socket?.on('connect_error', async (err) => {
-    console.log("Error establishing socket", err)
-    await fetch('/api/socket')
-  })
 
   return (
     <SocketContext.Provider value={socket}>{children}</SocketContext.Provider>
